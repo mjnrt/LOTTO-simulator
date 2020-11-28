8821f5c08e19279d200ba2836ed540b2c7c8c47d
Patryk <patryk_mej@o2.pl>
2020-11-28 12:35:26 +0100

    Lotto game v1

--- lotto.py
+++ lotto.py
@@ -1,5 +1,8 @@
 from random import randint
 
+print('--- LOTTO simulator ---')
+print('-- Pick 6 numbers in range of 1-49 --')
+
 lotto_numbers = []
 for _ in range(6):
     n = randint(1, 49)
@@ -10,14 +13,25 @@ for _ in range(6):
 converting = True
 while converting:
     try:
-        player_numbers_str = input('Input you 6 numbers (separator is ",": ')
+        player_numbers_str = input('Input your 6 numbers (separator is ",": ')
         player_numbers = player_numbers_str.split(',')
-        for i in range(6):
-            player_numbers[i] = int(player_numbers[i])
+        int_list = []
+        for i in player_numbers:
+            if int(i) <= 49 and int(i) >= 1:
+                i = int(i)
+                int_list.append(i)
+            else:
+                raise ValueError('Not in range 1-49')
         converting = False
     except ValueError:
-        print('Number you picked is a letter or float. Try again..')
+        print('Number not in range or letter. Try again..')
 
-print(lotto_numbers)
-print(player_numbers)
-print(player_numbers in lotto_numbers)
+print(f"Numbers you've picked: {int_list}")
+print(f'Winning numbers: {lotto_numbers}')
+
+win_numbers = []
+for number in int_list:
+    if number in lotto_numbers:
+        win_numbers.append(number)
+
+print(f'Numbers that match: {len(win_numbers)}')
